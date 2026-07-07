7b025c11be4209f3c1789dfd7a112e6667889b34
piniol111 <michal.pienkowski1@gmail.com>
2026-07-07 23:53:01 +0200

    naprawa buga json i aktualizacja testow

--- hello_world/formater.py
+++ hello_world/formater.py
@@ -1,4 +1,4 @@
-
+import json
 PLAIN = "plain"
 PLAIN_UP = "plain_uppercase"
 PLAIN_LO = "plain_lowercase"
@@ -21,8 +21,7 @@ def get_formatted(msg, imie, format):
 
 
 def format_to_json(msg, imie):
-    return ('{ "imie":"' + imie + '", "mgs":' +
-            msg + '"}')
+    return json.dumps({"imie": imie, "msg": msg})
 
 
 def plain_text(msg, imie):
