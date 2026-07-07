179d09b23b9268dc010e33ba25e4ecf89da26ca1
piniol111 <michal.pienkowski1@gmail.com>
2026-07-08 00:15:01 +0200

    zepsucie kodu przez usuniecie importow

--- hello_world/views.py
+++ hello_world/views.py
@@ -1,7 +1,7 @@
 from hello_world import app
 from hello_world.formater import get_formatted
-from hello_world.formater import SUPPORTED, PLAIN
-from flask import request
+#from hello_world.formater import SUPPORTED, PLAIN
+#from flask import request
 
 moje_imie = "Michal"
 msg = "Hello World!"
